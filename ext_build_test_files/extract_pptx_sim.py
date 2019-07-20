80be1760ee1be102eb2e3646f2548ab25be3523a
davidmltz <davidmltz@gmail.com>
2019-07-20 12:54:09 -0700

    Clean up directory (unused files) and clean up files

--- ext_build_test_files/extract_pptx_sim.py
+++
@@ -1,9 +0,0 @@
-# Build sim for extraction_pptx.py
-
-def read_file():
-    """Build simulated extraction_pptx.py
-       for entry script build
-    """
-    print(f"I have just run read_file from"
-          f" function call in extract_tool.py"
-        )
