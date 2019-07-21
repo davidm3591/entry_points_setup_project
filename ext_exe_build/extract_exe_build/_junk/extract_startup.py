e8ebd8105e20de6f37a5a6682e90cbde310c8f52
davidmltz <davidmltz@gmail.com>
2019-07-20 17:47:13 -0700

    Add lightweight files to test executable build

---
+++ ext_exe_build/extract_exe_build/_junk/extract_startup.py
@@ -0,0 +1,5 @@
+# set up a function to test __main__.py
+
+
+def extract_open():
+    print("I fired from a call to extract_open() in __main__.py")
---
+++ ext_exe_build/extract_exe_build/extraction_out.py
@@ -0,0 +1,16 @@
+# Simulate extraction_out.py for exe build
+import sys
+
+
+def ppt_stuff_out():
+    pptx_stuff = [
+        "\nPowerPoint Content Simulation:\n"
+        "\tMain page content",
+        "\tStart note slide content",
+        "\tMore note slide content",
+        "\tEnd note slide content\n"
+    ]
+    for slide in pptx_stuff:
+        print(slide)
+
+    sys.exit()
