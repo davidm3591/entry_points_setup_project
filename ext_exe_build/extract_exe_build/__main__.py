f7fda4cc4b04ce7adf3bbb374be8b38ee2257e58
davidmltz <davidmltz@gmail.com>
2019-07-20 17:47:13 -0700

    qqqqq q q q q qqqqq
    exit

---
+++ ext_exe_build/extract_exe_build/__main__.py
@@ -0,0 +1,14 @@
+import sys
+from extraction_tool import extract_open
+
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    extract_open()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ext_exe_build/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(name='extract_exe_build',
+      version='0.1.0',
+      packages=['extract_exe_build'],
+      entry_points={
+          'console_scripts': [
+              'extract_exe_build = extract_exe_build.__main__:main'
+          ]
+      },
+      )
+
+# setup(name='extract_build',
+#       version='0.1.0',
+#       packages=['extract_build'],
+#       entry_points={
+#           'console_scripts': [
+#               'extract_build = extract_build.__main__:main'
+#           ]
+#       },
+#       )
+
+# You must use setuptools, otherwise this won’t work.
+# The most important piece of code is the entry_points declaration (unsurprisingly).
+# The declaration reads
+
+# "name_of_executable = module.with:function_to_execute"
+
+# If you are developing a GUI application (in Tkinter,
+# PyQt/PySide, wxPython, PyGTK, PyGame…), you should
+# change the declaration to gui_scripts. On *nix,
+# this makes no difference, but on Windows, it means
+# that running your script by opening the created
+# .exe files does not show a console window. Note
+# that stdout/stderr do not work in that mode under
+# Windows, which can lead to spurious application
+# crashes. (GUI-only processes cannot use stdout/stderr
+# because they don’t have a console attached).
+#
+# You can create multiple scripts this way. You can
+# also have multiple console_scripts and gui_scripts
+# in one setup file.
